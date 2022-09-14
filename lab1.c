ecb921971b474398ae33963bb30d5995a3e6eeed
Jovanni Schneider <jovanni.schneider@usach.cl>
2022-09-14 01:15:24 -0300

    xd2

--- lab1.c
+++ lab1.c
@@ -18,28 +18,26 @@ int main(int argc, char * argv[]) {
         }
         int fd[2];
         int pid;
-        int buffer;
+        int buffer = 0;
         int totalYears = 5; //Es solo de ejemplo, se debe calcular
-        buffer = 0;
+        int stat;
         if(pipe(fd) == -1){
             printf("error\n");
             exit(-1);
         }
-        for (int i = 0; i < totalYears; ++i) {
-            if ((pid = fork()) == -1){
-                printf("error\n");
-                exit(-1);
-            }
-            if(pid > 0){
-                close(fd[0]);
+        for (int i = 0; i < totalYears; i++) {
+            pid = fork();
+            if(pid>0){
                 buffer+=1;
-                write(fd[1], &buffer,sizeof(buffer));
-                exit(0);
-            }else{
+                printf("buffer: %d\n", buffer);
+                write(fd[1],&buffer, sizeof(buffer));
+                wait(NULL);
+            }
+            if(pid == 0){
                 close(fd[1]);
                 read(fd[0],&buffer,sizeof(buffer));
-                printf(" soy el hijo numero %d\n",buffer);
-                break;
+                printf("soy el hijo %d pid: %d\n", buffer,getpid());
+                exit(buffer);
             }
         }
     }else {
