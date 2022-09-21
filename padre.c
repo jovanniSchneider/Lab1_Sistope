15918f9571276eb444a9f11743539e77ffdb20f8
Jovanni Schneider <jovanni.schneider@usach.cl>
2022-09-20 23:03:54 -0300

    Changes by jtoro

--- padre.c
+++ padre.c
@@ -20,12 +20,23 @@ TDAlista ** leerCSV(char* nombreArchivo){
     fclose(fp);
     return hash;
 }
-//crear el archivo intermedio
-
-//Capturar el puntero posicion del archivo
-
-//crear hijos (1 por year)
-
-void procesarDatos(){
-
+//Entradas TDALista** que simula una tabla hash,char* para el nombre del archivo
+//Salidas int que representa la cantidad de years que si tienen juegos
+//Descripcion crea un archivo que contiene la info de la tabla hash con el nombre ingresado, ademas de calcular
+//             los years con juegos, tambien dejando constancia de donde comienza cada year mediante ftell
+int crearArchivo(TDAlista** hash, char * nombreSalida){
+    int largoHash = getActualYear()-1985;
+    FILE * fp;
+    fp = fopen(nombreSalida, "w");
+    int cont = 0;
+    for(int i=0;i<(2022-1985);i++){
+        if(!esListaVacia(hash[i])) {
+            cont++;
+            printf("ftell: %d\n",ftell(fp));//esto deja en cola del pipe la actual posicion del puntero
+            recorrerLista(hash[i], fp);
+        }
+        liberarLista(hash[i]);
+    }
+    fclose(fp);
+    return cont;
 }
